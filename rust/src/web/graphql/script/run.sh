8c2c448358a6c0342def7451dacf36282392c3fe
Jonathan Hult <Jonathan@JonathanHult.com>
2024-03-26 10:05:41 -0500

    Script - comment out extraneous stuff
    extra

--- rust/src/web/graphql/script/run.sh
+++ rust/src/web/graphql/script/run.sh
@@ -29,29 +29,29 @@ readonly SCHEMA_FILE="$SCHEMA_FOLDER/mina-explorer.$EXT"
 readonly CONFIG_FILE="config.toml"
 
 # https://github.com/Granola-Team/mina-block-explorer/tree/main/graphql/queries
-readonly query_files=(
-    "account_dialog"
-    "blocks"
-    "fee_transfers"
-    "next_stakes"
-    "snarks"
-    "stakes"
-    "transactions"
-)
+# readonly query_files=(
+#     "account_dialog"
+#     "blocks"
+#     "fee_transfers"
+#     "next_stakes"
+#     "snarks"
+#     "stakes"
+#     "transactions"
+# )
 
 download() {
     curl --output "$SCHEMA_FILE" "$GH_EXPLORER_GRAPHQL/schemas/mina-explorer.$EXT"
     cat "$SCHEMA_FOLDER/schema_base.graphql" >> "$SCHEMA_FILE"
 
-    curl --output "$SCHEMA_FOLDER/mina-protocol.json" "$GH/MinaProtocol/mina/develop/graphql_schema.json"
+    # curl --output "$SCHEMA_FOLDER/mina-protocol.json" "$GH/MinaProtocol/mina/develop/graphql_schema.json"
 
     # query files
-    rm -rf "combined.$EXT"
-    for file in "${query_files[@]}"; do
-        local path="queries/$file.$EXT"
-        curl --output "../$path" "$GH_EXPLORER_GRAPHQL/$path"
-        cat "../$path" >> "../queries/combined.$EXT"
-    done
+    # rm -rf "combined.$EXT"
+    # for file in "${query_files[@]}"; do
+    #    local path="queries/$file.$EXT"
+    #    curl --output "../$path" "$GH_EXPLORER_GRAPHQL/$path"
+    #    cat "../$path" >> "../queries/combined.$EXT"
+    # done
 }
 
 generate() {
