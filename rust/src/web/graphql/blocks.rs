d05ae9a16c09ee027bc020fee82fb1acf878806d
Jonathan Hult <Jonathan@JonathanHult.com>
2024-03-26 22:07:47 -0500

    Use async-graphql-reverse
    https://github.com/tacogips/async-graphql-reverse

---
+++ rust/src/web/graphql/blocks.rs
@@ -0,0 +1,108 @@
+use super::gen::Block;
+use crate::{
+    block::precomputed::PrecomputedBlock,
+    proof_systems::signer::pubkey::CompressedPubKey,
+    protocol::serialization_types::{
+        common::{Base58EncodableVersionedType, HashV1},
+        version_bytes,
+    },
+    web::graphql::millis_to_date_string,
+};
+
+pub struct LedgerHash(pub String);
+
+impl LedgerHash {
+    pub fn from_hashv1(hashv1: HashV1) -> Self {
+        let versioned: Base58EncodableVersionedType<{ version_bytes::LEDGER_HASH }, _> =
+            hashv1.into();
+        Self(versioned.to_base58_string().unwrap())
+    }
+}
+
+impl From<PrecomputedBlock> for Block {
+    fn from(block: PrecomputedBlock) -> Self {
+        let winner_account = block.block_creator();
+        let date_time = millis_to_date_string(block.timestamp().try_into().unwrap());
+        let pk_creator = block.consensus_state().block_creator;
+        let creator = CompressedPubKey::from(&pk_creator).into_address();
+        let scheduled_time = block.scheduled_time.clone();
+        let received_time = millis_to_date_string(scheduled_time.parse::<i64>().unwrap());
+        let previous_state_hash = block.previous_state_hash().0;
+        let tx_fees = block.tx_fees();
+        let snark_fees = block.snark_fees();
+        let utc_date = block
+            .protocol_state
+            .body
+            .t
+            .t
+            .blockchain_state
+            .t
+            .t
+            .timestamp
+            .t
+            .t
+            .to_string();
+
+        let blockchain_state = block.protocol_state.body.t.t.blockchain_state.clone().t.t;
+        let snarked_ledger_hash =
+            LedgerHash::from_hashv1(blockchain_state.clone().snarked_ledger_hash).0;
+        let staged_ledger_hashv1 = blockchain_state
+            .staged_ledger_hash
+            .t
+            .t
+            .non_snark
+            .t
+            .ledger_hash;
+        let staged_ledger_hash = LedgerHash::from_hashv1(staged_ledger_hashv1).0;
+
+        // consensus state
+        let consensus_state = block.protocol_state.body.t.t.consensus_state.clone().t.t;
+
+        let total_currency = consensus_state.total_currency.t.t;
+        let blockchain_length = block.blockchain_length;
+        let block_height = blockchain_length;
+        let epoch_count = consensus_state.epoch_count.t.t;
+        let epoch = epoch_count;
+        let has_ancestor_in_same_checkpoint_window =
+            consensus_state.has_ancestor_in_same_checkpoint_window;
+        let last_vrf_output = block.last_vrf_output();
+        let min_window_density = consensus_state.min_window_density.t.t;
+        let slot_since_genesis = consensus_state.global_slot_since_genesis.t.t;
+        let slot = consensus_state.curr_global_slot.t.t.slot_number.t.t;
+        todo!()
+        // Block {
+        //     state_hash: block.state_hash,
+        //     block_height: block.blockchain_length,
+        //     date_time,
+        //     winner_account: BlockWinnerAccount { winner_account },
+        //     creator_account: CreatorAccount {
+        //         public_key: creator.clone(),
+        //     },
+        //     creator,
+        //     received_time,
+        //     protocol_state: ProtocolState {
+        //         previous_state_hash,
+        //         blockchain_state: BlockchainState {
+        //             date: utc_date.clone(),
+        //             utc_date,
+        //             snarked_ledger_hash,
+        //             staged_ledger_hash,
+        //         },
+        //         consensus_state: ConsensusState {
+        //             total_currency,
+        //             blockchain_length,
+        //             block_height,
+        //             epoch,
+        //             epoch_count,
+        //             has_ancestor_in_same_checkpoint_window,
+        //             last_vrf_output,
+        //             min_window_density,
+        //             slot,
+        //             slot_since_genesis,
+        //         },
+        //     },
+        //     tx_fees: tx_fees.to_string(),
+        //     snark_fees: snark_fees.to_string(),
+        // }
+    }
+}
--- rust/src/web/graphql/mod.rs
+++ rust/src/web/graphql/mod.rs
@@ -1,10 +1,45 @@
 pub mod blocks;
+mod gen;
+mod query_implementations;
+mod transactions;
 
-use actix_web::HttpResponse;
-use async_graphql::http::GraphiQLSource;
+use self::gen::{schema_builder, Query};
+use crate::store::IndexerStore;
+use actix_web::{http::header::ContentType, HttpResponse};
+use async_graphql::{http::GraphiQLSource, Context, EmptySubscription, Schema};
+use chrono::{DateTime, SecondsFormat};
+use std::sync::Arc;
 
-pub async fn index_graphiql() -> actix_web::Result<HttpResponse> {
+pub const ENDPOINT_GRAPHQL: &str = "/graphql";
+
+pub(crate) async fn graphiql() -> actix_web::Result<HttpResponse> {
     Ok(HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(GraphiQLSource::build().endpoint("/graphql").finish()))
+        .content_type(ContentType::html())
+        .body(GraphiQLSource::build().endpoint(ENDPOINT_GRAPHQL).finish()))
+}
+
+pub(crate) fn build_schema(
+    store: Arc<IndexerStore>,
+) -> Schema<Query, EmptyMutation, EmptySubscription> {
+    schema_builder().data(store).finish()
+}
+
+pub(crate) fn db<'ctx>(ctx: &Context) -> &'ctx Arc<IndexerStore> {
+    ctx.data::<Arc<IndexerStore>>()
+        .expect("Database should be in the context")
+}
+
+/// convert epoch millis to an ISO 8601 formatted date
+pub(crate) fn millis_to_date_string(millis: i64) -> String {
+    let date_time = DateTime::from_timestamp_millis(millis).unwrap();
+    date_time.to_rfc3339_opts(SecondsFormat::Millis, true)
+}
+
+// JSON utility
+pub(crate) fn sanitize_json<T: serde::Serialize>(s: T) -> String {
+    serde_json::to_string(&s).unwrap().replace('\"', "")
+}
+
+pub(crate) fn sanitize_json_option<T: serde::Serialize>(s: T) -> Option<String> {
+    Option::from(sanitize_json(s))
 }
